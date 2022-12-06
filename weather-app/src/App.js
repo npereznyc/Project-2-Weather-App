aa09d14d1d07ef78ea6868d99ab641af9f2ce61a
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-05 18:30:21 -0600

    Changed h1

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Let Project 2 Commence</h1>
+      <h1>Weather App</h1>
       <h2>By Natalie and Emily</h2>
     </div>
   );
