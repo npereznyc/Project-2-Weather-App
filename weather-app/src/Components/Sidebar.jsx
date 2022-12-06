d959cda4e387d412db8516ad5f4e4c02f5e73ec7
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-06 15:05:16 -0600

    updated sidebar url to include more weather details

--- weather-app/src/Components/Sidebar.jsx
+++ weather-app/src/Components/Sidebar.jsx
@@ -14,10 +14,10 @@ import React, {useState, useEffect} from 'react'
     const lat = 41.85
     async function getTemp(){
         try{
-            const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weathercode&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`)
+            const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=apparent_temperature&daily=temperature_2m_max,temperature_2m_min&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`)
             const tempData = await response.json()
             setTemp(tempData)
-            console.log(tempData.current_weather.weathercode)
+            console.log(tempData)
         }catch(err){
             console.log(err)
         }
