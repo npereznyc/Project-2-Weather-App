f7d658d504fed961580df7f1e9aa7adbc2d3ce50
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-08 14:47:24 -0600

    we are able to grab longitude and latitude from app.js and trans fer it to Sidebar.js and it appears on the screen but there is a 400 error.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,6 +7,7 @@ import WeeklyForecast from './Components/WeeklyForecast';
 import {useEffect, useState} from 'react'
 
 function App() {
+  getLocation()
   const [lat, setLat] = useState([])
   const [long, setLong] = useState([])
   const[tempData, setTempData] = useState([])
@@ -16,37 +17,39 @@ function App() {
   // } else {
   //   console.log("Not Available");
   // }
-  async function getLocation() {
-    
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude)
-      setLong(position.coords.longitude)
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
-    });
-    try{
-      const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}1&hourly=apparent_temperature&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`)
-      const data = await response.json()
-      setTempData(data)
-      console.log(data)//DOES PRINT OUT DATA
-    }catch(err){
-      console.log(err)
+    function getLocation(){
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
+        console.log("Latitude is :", position.coords.latitude)
+        console.log("Longitude is :", position.coords.longitude)
+      })
     }
-  }
+    async function getData() {
+      try{
+        const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`)
+        const data = await response.json()
+        setTempData(data)
+        console.log(data)//DOES PRINT OUT DATA
+      }catch(err){
+        console.log(err)
+      }
+    }
 
   useEffect(()=> {
-    getLocation()
-  }, [parseFloat(lat), parseFloat(long)])
-
-  console.log(tempData.current_weather.temperature)//DOES PRINT OUT DATA- at first it works but when we refresh it throws error
+      getLocation()
+  }, [])
+  
+  useEffect(()=> {
+    getData()
+  }, [lat, long])
+  // console.log(tempData)//DOES PRINT OUT DATA- at first it works but when we refresh it throws error
   return (
     tempData
     ?
     <div className="App">
       {/* <h1>{tempData.current_weather.weathercode}</h1> */}
-      {/* <Sidebar 
-       weatherData={tempData}
-      /> */}
+      <Sidebar weatherData={tempData} />
       {/* <Wind /> */}
       {/* <SunriseSunset /> */}
       {/* <WeeklyForecast /> */}
--- weather-app/src/Components/Sidebar.jsx
+++ weather-app/src/Components/Sidebar.jsx
@@ -40,20 +40,30 @@ import React, {useState, useEffect} from 'react'
         [96, "Thunderstorms with Hail"],
         [99, "Thunderstorms with Hail"],
     ])
+    // console.log(weatherData.current_weather.temperature)
+    if(!weatherData){
+        return (
+            <div>Loading...</div>
+        )
+    }
+    if(weatherData){
+        return (
+                <div>
+                    <div>Test</div>
+                    <h1>Current Temperature: {weatherData?.current_weather?.temperature}</h1>
+                        {/* <div className="Sidebar">
+                        {weatherCodeHashmap.get(weatherData.current_weather.weathercode)}
+                        <div className="Date">Date & Time: {weatherData.current_weather.time}</div>
+                        <div className="CurrentTemperature">Current Temperature: {weatherData.current_weather.temperature}°F</div>
+                        <div className="FeelsLike">Feels Like: {weatherData.hourly.apparent_temperature[0]}°F</div>
+                        <div className="MaxAndMinTemp">High: {weatherData.daily.temperature_2m_max[0]}°F  Low: {weatherData.daily.temperature_2m_min[0]}°F</div>
+                    </div>  */}
+                </div>
+            
+            )
 
-    return (
-        <div>
-            <div>Test</div>
-            <div className="Sidebar">
-                {weatherCodeHashmap.get(weatherData.current_weather.weathercode)}
-                <div className="Date">Date & Time: {weatherData.current_weather.time}</div>
-                <div className="CurrentTemperature">Current Temperature: {weatherData.current_weather.temperature}°F</div>
-                <div className="FeelsLike">Feels Like: {weatherData.hourly.apparent_temperature[0]}°F</div>
-                <div className="MaxAndMinTemp">High: {weatherData.daily.temperature_2m_max[0]}°F  Low: {weatherData.daily.temperature_2m_min[0]}°F</div>
-            </div> 
-        </div>
-       
-    )
+    }
+    
 }
 
 export default Sidebar
