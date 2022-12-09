f35340822c4250171a05ad694099bd9106a24734
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-08 19:26:02 -0600

    used <br/> to indent after text in forecast component

--- weather-app/src/Components/WeeklyForecast.jsx
+++ weather-app/src/Components/WeeklyForecast.jsx
@@ -35,27 +35,27 @@ const WeeklyForecast = ({weatherData}) => {
             <div className="ForecastContainer">
                 <div className="ForecastHeader"><h2>Weekly Forecast</h2></div>
                 <div className="day1">
-                    <h3>{weatherData?.daily?.time[1]}:{'\n'}
+                    <h3>{weatherData?.daily?.time[1]}:<br />
                     {weatherCodeHashmap.get(weatherData?.daily?.weathercode[1])}</h3>
                 </div>
                 <div className="day2">
-                    <h3>{weatherData?.daily?.time[2]}:{'\n'}
+                    <h3>{weatherData?.daily?.time[2]}:<br />
                     {weatherCodeHashmap.get(weatherData?.daily?.weathercode[2])}</h3>
                 </div>
                 <div className="day3">
-                    <h3>{weatherData?.daily?.time[3]}:{'\n'}
+                    <h3>{weatherData?.daily?.time[3]}:<br />
                     {weatherCodeHashmap.get(weatherData?.daily?.weathercode[3])}</h3>
                 </div>
                 <div className="day4">
-                    <h3>{weatherData?.daily?.time[4]}:{'\n'}
+                    <h3>{weatherData?.daily?.time[4]}:<br />
                     {weatherCodeHashmap.get(weatherData?.daily?.weathercode[4])}</h3>
                 </div>
                 <div className="day5">
-                    <h3>{weatherData?.daily?.time[5]}:{'\n'}
+                    <h3>{weatherData?.daily?.time[5]}:<br />
                     {weatherCodeHashmap.get(weatherData?.daily?.weathercode[5])}</h3>
                 </div>
                 <div className="day6">
-                    <h3>{weatherData?.daily?.time[6]}:{'\n'}
+                    <h3>{weatherData?.daily?.time[6]}:<br />
                     {weatherCodeHashmap.get(weatherData?.daily?.weathercode[6])}</h3>
                 </div>
 
