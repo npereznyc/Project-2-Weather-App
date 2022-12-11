9f6c91eecccf46ac4e290f30465211e9ff2ed910
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-11 17:15:39 -0600

    completed CSS styling for phone fit

--- src/Components/WeeklyForecast.jsx
+++ src/Components/WeeklyForecast.jsx
@@ -43,27 +43,27 @@ const WeeklyForecast = ({weatherData}) => {
                 </Link>
                 <div className="day1">
                     <h3>{weatherData?.daily?.time[1]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[1])}</h3>
+                    <span className="Weathercode">{weatherCodeHashmap.get(weatherData?.daily?.weathercode[1])}</span></h3>
                 </div>
                 <div className="day2">
                     <h3>{weatherData?.daily?.time[2]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[2])}</h3>
+                    <span className="Weathercode">{weatherCodeHashmap.get(weatherData?.daily?.weathercode[2])}</span></h3>
                 </div>
                 <div className="day3">
                     <h3>{weatherData?.daily?.time[3]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[3])}</h3>
+                    <span className="Weathercode">{weatherCodeHashmap.get(weatherData?.daily?.weathercode[3])}</span></h3>
                 </div>
                 <div className="day4">
                     <h3>{weatherData?.daily?.time[4]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[4])}</h3>
+                    <span className="Weathercode">{weatherCodeHashmap.get(weatherData?.daily?.weathercode[4])}</span></h3>
                 </div>
                 <div className="day5">
                     <h3>{weatherData?.daily?.time[5]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[5])}</h3>
+                    <span className="Weathercode">{weatherCodeHashmap.get(weatherData?.daily?.weathercode[5])}</span></h3>
                 </div>
                 <div className="day6">
                     <h3>{weatherData?.daily?.time[6]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[6])}</h3>
+                    <span className="Weathercode">{weatherCodeHashmap.get(weatherData?.daily?.weathercode[6])}</span></h3>
                 </div>
             </div>
         )
