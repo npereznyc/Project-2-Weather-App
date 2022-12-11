fa88c3b73ba80d8a95441298feb0331ebf4a631f
npereznyc <natalieperez84@gmail.com>
2022-12-11 10:19:52 -0500

    Added emojis to the weathercode

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,67 +1,41 @@
-import React from "react";
 import LiveClock from "react-live-clock";
 
 const Sidebar = ({weatherData}) => {
     let today = new Date()
-    let year = today.getFullYear();
-    let month = today.getMonth() + 1;
     let day = today.getDay();
-    let date = today.getDate();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let second = today.getSeconds(); 
     let dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let session = "AM"
-    
-    //gives common time format not military time
-    if (hour === 0){
-        hour = 12
-    }
-    //defines if AM or PM
-    if (hour > 12){
-        session = "PM"
-        hour = hour-12
-    }
-    //minutes have double digits
-    if (min < 10){
-        min = "0" + min
-    }
-    let time = (hour + ":" + min + session)
-
-    
-    
-    
+ 
 
     //defines weathercode 
-    let weatherCodeHashmap = new Map([
-        [0, "Clear"],
-        [1, "Partly Cloudy"],
-        [2, "Partly Cloudy"],
-        [3, "Partly Cloudy"],
-        [45, "Foggy"],
-        [48, "Foggy"],
-        [51, "Drizzle"],
-        [53, "Drizzle"],
-        [55, "Drizzle"],
-        [56, "Freezing Drizzle"],
-        [57, "Freezing Drizzle"],
-        [61, "Rain"],
-        [63, "Rain"],
-        [66, "Rain"],
-        [66, "Freezing Rain"],
-        [67, "Freezing Rain"],
-        [71, "Snow"],
-        [73, "Snow"],
-        [75, "Snow"],
-        [77, "Snow Grain"],
-        [80, "Rain Showers"],
-        [81, "Rain Showers"],
-        [82, "Rain Showers"],
-        [85, "Snow Showers"],
-        [86, "Snow Showers"],
-        [95, "Thunderstorms with Hail"],
-        [96, "Thunderstorms with Hail"],
-        [99, "Thunderstorms with Hail"],
+  let weatherCodeHashmap = new Map([
+        [0, "🌤 Clear"], 
+        [1, "🌥 Partly Cloudy"],  
+        [2, "🌥 Partly Cloudy"],
+        [3, "🌥 Partly Cloudy"], 
+        [45, "🌫 Foggy"],
+        [48, "🌫 Foggy"],  
+        [51, "☔ Drizzle"],
+        [53, "☔ Drizzle"], 
+        [55, "☔ Drizzle"],
+        [56, "☔ Freezing Drizzle"],
+        [57, "☔ Freezing Drizzle"],
+        [61, "🌧 Rain"],  
+        [63, "🌧 Rain"],
+        [66, "🌧 Rain"],
+        [66, "🌧 Freezing Rain"],
+        [67, "🌧 Freezing Rain"],
+        [71, "❄ Snow"], 
+        [73, "❄ Snow"],
+        [75, "❄ Snow"],
+        [77, "❄ Snow Grain"],
+        [80, "☔ Rain Showers"],  
+        [81, "☔ Rain Showers"], 
+        [82, "☔ Rain Showers"],
+        [85, "❄ Snow Showers"],
+        [86, "❄ Snow Showers"],
+        [95, "⛈ Thunderstorms with Hail"],
+        [96, "⛈ Thunderstorms with Hail"],
+        [99, "⛈ Thunderstorms with Hail"],
     ])
     
     //waits for weather data from location 
@@ -70,7 +44,7 @@ const Sidebar = ({weatherData}) => {
             <div>Loading...</div>
         )
     }
-    if(weatherData){
+    if(weatherData && weatherData.current_weather && weatherData.hourly && weatherData.daily){
         return (
             <div className="Sidebar">
                 <div>
@@ -78,16 +52,16 @@ const Sidebar = ({weatherData}) => {
                         <h3>{dayList[day]}</h3>
                     </div>
                     <div className="Date">
-                        <LiveClock className="Date" format={'hh:mma'} ticking={true}/>
+                        <LiveClock className="Date"  format={'h:mma'}  ticking={true}/>
                     </div>
                     <div className="Condition">
-                        <h2>{weatherCodeHashmap.get(weatherData?.current_weather?.weathercode)}</h2></div>
+                        <h2>{weatherCodeHashmap.get(weatherData.current_weather.weathercode)}</h2></div>
                     <div className="CurrentTemperature">
-                        <h2>Current Temperature: {weatherData?.current_weather?.temperature}°F</h2></div>
+                        <h2>Current Temperature: {weatherData.current_weather.temperature}°F</h2></div>
                     <div className="FeelsLike">
-                        <h2>Feels Like: {weatherData?.hourly?.apparent_temperature[0]}°F</h2></div>
+                        <h2>Feels Like: {weatherData.hourly.apparent_temperature[0]}°F</h2></div>
                     <div className="MaxAndMinTemp">
-                        <h2>High: {weatherData?.daily?.temperature_2m_max[0]}°F  Low: {weatherData?.daily?.temperature_2m_min[0]}°F</h2>
+                        <h2>High: {weatherData.daily.temperature_2m_max[0]}°F  Low: {weatherData?.daily?.temperature_2m_min[0]}°F</h2>
                     </div>
                 </div> 
             </div>
--- src/Components/WeeklyForecast.jsx
+++ src/Components/WeeklyForecast.jsx
@@ -4,38 +4,38 @@ const WeeklyForecast = ({weatherData}) => {
     
     //defines weathercode   
     let weatherCodeHashmap = new Map([
-        [0, "Clear"],
-        [1, "Partly Cloudy"],
-        [2, "Partly Cloudy"],
-        [3, "Partly Cloudy"],
-        [45, "Foggy"],
-        [48, "Foggy"],
-        [51, "Drizzle"],
-        [53, "Drizzle"],
-        [55, "Drizzle"],
-        [56, "Freezing Drizzle"],
-        [57, "Freezing Drizzle"],
-        [61, "Rain"],
-        [63, "Rain"],
-        [66, "Rain"],
-        [66, "Freezing Rain"],
-        [67, "Freezing Rain"],
-        [71, "Snow"],
-        [73, "Snow"],
-        [75, "Snow"],
-        [77, "Snow Grain"],
-        [80, "Rain Showers"],
-        [81, "Rain Showers"],
-        [82, "Rain Showers"],
-        [85, "Snow Showers"],
-        [86, "Snow Showers"],
-        [95, "Thunderstorms with Hail"],
-        [96, "Thunderstorms with Hail"],
-        [99, "Thunderstorms with Hail"],
+        [0, "🌤 Clear"], 
+        [1, "🌥 Partly Cloudy"],  
+        [2, "🌥 Partly Cloudy"],
+        [3, "🌥 Partly Cloudy"], 
+        [45, "🌫 Foggy"],
+        [48, "🌫 Foggy"],  
+        [51, "☔ Drizzle"],
+        [53, "☔ Drizzle"], 
+        [55, "☔ Drizzle"],
+        [56, "☔ Freezing Drizzle"],
+        [57, "☔ Freezing Drizzle"],
+        [61, "🌧 Rain"],  
+        [63, "🌧 Rain"],
+        [66, "🌧 Rain"],
+        [66, "🌧 Freezing Rain"],
+        [67, "🌧 Freezing Rain"],
+        [71, "❄ Snow"], 
+        [73, "❄ Snow"],
+        [75, "❄ Snow"],
+        [77, "❄ Snow Grain"],
+        [80, "☔ Rain Showers"],  
+        [81, "☔ Rain Showers"], 
+        [82, "☔ Rain Showers"],
+        [85, "❄ Snow Showers"],
+        [86, "❄ Snow Showers"],
+        [95, "⛈ Thunderstorms with Hail"],
+        [96, "⛈ Thunderstorms with Hail"],
+        [99, "⛈ Thunderstorms with Hail"],
     ])
     
     //provides wind speed and direction for the next 7 days
-    if(weatherData){
+    if(weatherData && weatherData.daily){
         return (
             <div className="ForecastContainer">
                 <Link to={`/weekly`} className="ForecastHeader">
@@ -44,28 +44,28 @@ const WeeklyForecast = ({weatherData}) => {
                     </div>
                 </Link>
                 <div className="day1">
-                    <h3>{weatherData?.daily?.time[1]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[1])}</h3>
+                    <h3>{weatherData.daily.time[1]}:<br />
+                    {weatherCodeHashmap.get(weatherData.daily.weathercode[1])}</h3>
                 </div>
                 <div className="day2">
-                    <h3>{weatherData?.daily?.time[2]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[2])}</h3>
+                    <h3>{weatherData.daily.time[2]}:<br />
+                    {weatherCodeHashmap.get(weatherData.daily.weathercode[2])}</h3>
                 </div>
                 <div className="day3">
-                    <h3>{weatherData?.daily?.time[3]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[3])}</h3>
+                    <h3>{weatherData.daily.time[3]}:<br />
+                    {weatherCodeHashmap.get(weatherData.daily.weathercode[3])}</h3>
                 </div>
                 <div className="day4">
-                    <h3>{weatherData?.daily?.time[4]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[4])}</h3>
+                    <h3>{weatherData.daily.time[4]}:<br />
+                    {weatherCodeHashmap.get(weatherData.daily.weathercode[4])}</h3>
                 </div>
                 <div className="day5">
-                    <h3>{weatherData?.daily?.time[5]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[5])}</h3>
+                    <h3>{weatherData.daily.time[5]}:<br />
+                    {weatherCodeHashmap.get(weatherData.daily.weathercode[5])}</h3>
                 </div>
                 <div className="day6">
-                    <h3>{weatherData?.daily?.time[6]}:<br />
-                    {weatherCodeHashmap.get(weatherData?.daily?.weathercode[6])}</h3>
+                    <h3>{weatherData.daily.time[6]}:<br />
+                    {weatherCodeHashmap.get(weatherData.daily.weathercode[6])}</h3>
                 </div>
             </div>
         )
